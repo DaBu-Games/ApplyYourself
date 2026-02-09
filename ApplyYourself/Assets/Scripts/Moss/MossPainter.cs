58512053e53496fca95a961f6d19041ec0732b73
DaBu Games <buitelaardaan2004@gmail.com>
2026-02-09 15:16:03 +0100

    moss shader graph
    i made a shader graph i can put on the ground or floor. Now i make a full black texture 2d for every surface and draw a white line on that texture of where the player has been. This now reads out these white lines and changes those lines in to moss.

---
+++ ApplyYourself/Assets/Scripts/Moss/MossPainter.cs
@@ -0,0 +1,52 @@
+using System;
+using UnityEngine;
+
+public class MossPainter : MonoBehaviour
+{
+    [SerializeField] private Material brushMaterial;
+    [SerializeField] private float paintDistance = 0.3f;
+    [SerializeField] private float rayCastDistance = 1.5f;
+    [SerializeField] private float brushRadius = 0.05f;
+    [SerializeField] private float brushStrength = 1f;
+
+    private Vector3 lastPaintPos;
+    private MossSurface currentSurface;
+    private Collider currentCollider;
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, lastPaintPos) < paintDistance)
+            return;
+
+        if (!Physics.Raycast(
+                transform.position,
+                -transform.up,
+                out var hit,
+                rayCastDistance))
+            return;
+
+        if (hit.collider != currentCollider)
+        {
+            currentCollider = hit.collider;
+
+            if (!hit.collider.TryGetComponent(out currentSurface))
+            {
+                currentSurface = null;
+                return;
+            }
+        }
+        
+        if (!currentSurface)
+            return;
+        
+
+        currentSurface.PaintCircle(
+            hit.textureCoord,
+            brushRadius,
+            brushStrength
+        );
+
+        lastPaintPos = transform.position;
+    }
+
+}
