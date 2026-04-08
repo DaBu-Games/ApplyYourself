a6ceea02209a9918c32280b6801d71e122317dad
DaBu Games <buitelaardaan2004@gmail.com>
2026-04-08 13:28:24 +0200

    start screen

--- ApplyYourself/Assets/Scripts/Player/PlayerInput.cs
+++ ApplyYourself/Assets/Scripts/Player/PlayerInput.cs
@@ -1,7 +1,6 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerInput : MonoBehaviour
